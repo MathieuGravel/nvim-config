384c3afceb34a0a1c35d04d94ec75d894037c13d
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2022-08-14 15:12:42 -0400

    Rename theme for colorscheme

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -2,4 +2,7 @@
 -- vim.g.tokyonight_transparent = true
 
 vim.opt.background = "dark"
-vim.cmd("silent! colorscheme tokyonight")
+
+local colorscheme = 'tokyonight'
+
+vim.cmd("silent! colorscheme " .. colorscheme)
